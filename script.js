ac7ace4e2cf144d9186a5e8b6a8fdc01507df7a2
sabeeh <sabeehck02@gmail.com>
2025-09-19 22:49:47 +0530

    Commit 3.1

--- script.js
+++ script.js
@@ -4,14 +4,35 @@ const userId = document.querySelector("#userId");
 
 const getData = async (userId) => {
     const res = await fetch(`https://api.github.com/users/${userId}`);
-    const obj = await res.json();
-    return obj;
+    if (res.ok) {
+        const obj = await res.json();
+        return obj;
+    } else {
+        console.error("Invalid UserId");
+        return 0;
+    }
+};
+
+const showResult = (data) => {
+    console.log(data);
+    const container = document.querySelector("#result");
+    const newDiv = document.createElement("div");
+    newDiv.className = "div";
+    if (data === 0) newDiv.innerHTML = `<p> Invalid UserId </p>`;
+    else {
+        newDiv.innerHTML = `
+            <img src="${data.avatar_url}" class="avatar" width="48px"></img>
+            <p> Name: ${data.name} </p>
+        `;
+    }
+    container.textContent = "";
+    container.append(newDiv);
 };
 
 theForm.addEventListener("submit", async (event) => {
     event.preventDefault();
     const data = await getData(userId.value);
-    console.log(data);
+    if (data != 0) {
+        showResult(data);
+    } else showResult(data);
 });
-/* 
-getData("sabeeh-ck"); */
