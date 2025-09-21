9367b286ec5bc45744dddda3cc4adeb8a4e5cd47
sabeeh <sabeehck02@gmail.com>
2025-09-21 16:20:37 +0530

    Commit 5.1

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const theForm = document.querySelector("form");
 const userId = document.querySelector("#userId");
 let data;
 
+const avatar = document.querySelector("#avatar");
+const userName = document.querySelector("#name");
+const loginId = document.querySelector("#profileId");
+
 const getData = async (query) => {
     let link;
 
@@ -22,39 +26,27 @@ const getData = async (query) => {
     }
 };
 
-function showResult(data) {
-    console.log(data);
-    const container = document.querySelector("#result");
-    const name = document.createElement("p");
-    const avatar = document.createElement("img");
-    const followers = document.createElement("p");
-    const followersLink = document.createElement("a");
-
-    if (data === 0) name.textContent = "Invalid UserId";
+const showResult = (data) => {
+    if (data === 0) userName.textContent = "Invalid UserId";
     else {
         avatar.src = data.avatar_url;
-        avatar.className = "avatar";
-        avatar.width = 48;
-        name.textContent = data.name;
-        followers.textContent = `Followers: ${data.followers}`;
-        followersLink.href = "#";
-        followersLink.id = "followers_url";
-        followersLink.text = "Show Followers";
-        followersLink.addEventListener("click", mainFunction);
+        userName.textContent = data.name;
+        loginId.textContent = userId.value;
     }
-    container.textContent = "";
-    container.append(name, avatar, followers, followersLink);
-}
+};
 
 async function mainFunction(event) {
     event.preventDefault();
-    console.log(event.currentTarget.id);
+    //    console.log(event.currentTarget.id);
     let query;
 
     if (event.currentTarget.id === "theForm") query = "Details";
-    else if (event.currentTarget.id === "followers_url") query = "Followers";
+    else if (event.currentTarget.id === "followers") query = "Followers";
+    else if (event.currentTarget.id === "following") query = "Following";
 
     data = await getData(query);
+
+    console.log(data);
 
     showResult(data);
 }
